3b94c355e841e85aa367ecbdf80df3325c2f57ca
ShemanskyiM <mike.shemanskiy@gmail.com>
2017-01-11 22:58:07 +0200

    ex 2.1

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -13,4 +13,7 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+//KToF converts a Kelvin temperature to Fahrenheit
+func KToF(k Kelvin) Kelvin { return Kelvin(k*9/5 + 32) }
+
 //!-
--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -8,10 +8,12 @@ package tempconv
 
 import "fmt"
 
+type Kelvin float64
 type Celsius float64
 type Fahrenheit float64
 
 const (
+	AbsoluteZeroK Kelvin  = -273.15
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
@@ -19,5 +21,6 @@ const (
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
 
 //!-
